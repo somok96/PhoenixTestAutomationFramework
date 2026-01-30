6c0f1ebf5cd43c3e9df36fc47851f98dc86837ce
Somok Mukherjee <somokmukherjee58@gmail.com>
2026-01-30 22:52:28 +0530

    Created CustomerDao and CustomerDBModel for Requirement-13

---
+++ src/test/java/com/database/dao/DemoDaoRunner.java
@@ -0,0 +1,22 @@
+package com.database.dao;
+
+import java.sql.SQLException;
+
+import org.testng.Assert;
+
+import com.api.request.model.Customer;
+import com.database.model.CustomerDBModel;
+
+public class DemoDaoRunner {
+
+	public static void main(String[] args) throws SQLException {
+		
+		CustomerDBModel customerDBModel = CustomerDao.getCustomerInfo();
+		System.out.println(customerDBModel);
+		
+		Customer customer = new Customer("Florian", "Pagac", "8240967632", "", "somok@gmail.com", "");
+		Assert.assertEquals(customerDBModel.getFirst_name(), customer.first_name());
+		
+	}
+
+}
---
+++ src/test/java/com/database/model/DemoRunner.java
@@ -0,0 +1,15 @@
+package com.database.model;
+
+public class DemoRunner {
+	
+	public static void main(String[] args) {
+		
+		CustomerDBModel customer = new CustomerDBModel("Somok", "Mukherjee", "8240967643", "", "abc@yopmail.com", "abc@yopmail.com");
+		System.out.println(customer);
+		
+	}
+	
+	
+	
+
+}
