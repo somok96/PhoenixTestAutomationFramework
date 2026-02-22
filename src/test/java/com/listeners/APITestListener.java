222525162c5e05bc14024574b12a4c593999b705
Somok Mukherjee <somokmukherjee58@gmail.com>
2026-02-22 21:52:58 +0530

    Added Allure Report to the framework
    Created AllureEnvWriterUtility that shows env info in the report.
    
    Changed the ConfigManager env variable to public.

--- src/test/java/com/listeners/APITestListener.java
+++ src/test/java/com/listeners/APITestListener.java
@@ -8,6 +8,8 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.api.utils.AllureEnvironmentWriterUtil;
+
 public class APITestListener implements ITestListener {
 
 	private static Logger LOGGER = LogManager.getLogger();
@@ -42,6 +44,7 @@ public class APITestListener implements ITestListener {
 
 	public void onStart(ITestContext context) {
 		LOGGER.info("********************* Starting the Phoenix Framework *********************");
+		AllureEnvironmentWriterUtil.createEnvironmentPropertiesFile();
 	}
 
 	public void onFinish(ITestContext context) {
